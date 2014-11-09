5e88b0377f6005b039d6319d2904c8addcfc0054
Tom Shapira <tom.shapira@whitesourcesoftware.com>
2014-11-09 08:15:53 +0200

    Update agent version to "2.0", use agent api version 2.0.0 to compress diff string before sending it over the wire

--- src/main/java/org/whitesource/fs/Constants.java
+++ src/main/java/org/whitesource/fs/Constants.java
@@ -22,6 +22,6 @@ public final class Constants {
     public static final String PROXY_PASS_PROPERTY_KEY = "proxy.pass";
 
     public static final String AGENT_TYPE = "fs-agent";
-    public static final String AGENT_VERSION = "1.0";
+    public static final String AGENT_VERSION = "2.0";
 
 }
