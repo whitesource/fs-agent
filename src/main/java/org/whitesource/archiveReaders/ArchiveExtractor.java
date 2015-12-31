f9149f9f6f23be0337fb46fac37de7872247cb96
annarozin <anna.rozin@whitesourcesoftware.com>
2015-12-31 17:41:47 +0200

    Add support for extraction of Python archive files (.whl, egg, tar.gz, zip) and Ruby archives (.gem )

--- src/main/java/org/whitesource/archiveReaders/ArchiveExtractor.java
+++ src/main/java/org/whitesource/archiveReaders/ArchiveExtractor.java
@@ -5,17 +5,19 @@ import net.lingala.zip4j.exception.ZipException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.tools.ant.DirectoryScanner;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.archiver.tar.TarUnArchiver;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  *
- * The class supports recursive deCompression of compressed files (Java types - jar, ear and war).
+ * The class supports recursive deCompression of compressed files (Java, Python & Ruby types).
  * @author anna.rozin
  */
 public class ArchiveExtractor {
@@ -26,24 +28,55 @@ public class ArchiveExtractor {
 
     private static final String TEMP_FOLDER = System.getProperty("java.io.tmpdir") + "WhiteSource-ArchiveExtractor";
 
+    public static final List<String> ZIP_EXTENSIONS = Arrays.asList("jar", "war", "ear", "egg", "zip", "whl");
+    public static final List<String> RUBY_EXTENSIONS = Arrays.asList("gem");
+    public static final List<String> TAR_EXTENSIONS = Arrays.asList("tar.gz", "tar");
+
+    public static final String ZIP_EXTENSION_PATTERN ;
+    public static final String GEM_EXTENSION_PATTERN ;
+    public static final String TAR_EXTENSION_PATTERN ;
+    public static final String RUBY_DATA_FILE = "data.tar.gz";
+    public static final String TAR_SUFFIX = ".tar";
+    public static final String TAR_GZ_SUFFIX = TAR_SUFFIX + ".gz";
+
+    public static final String UN_ARCHIVER_LOGGER = "unArchiverLogger";
+    public static final String GLOB_PATTERN_PREFIX =  "**/*.";
     public static final String DOT = ".";
-    public static final String[] JAVA_EXTENSION = {"**/*.jar", "**/*.war", "**/*.ear"};
-    public static final String JAVA_EXTENSION_PATTERN = ".*\\.jar|.*\\.ear|.*\\.war";
+    public static final String BLANK = "";
+    public static final String PATTERN_PREFIX = ".*\\.";
+    public static final String OR = "|";
+
+    static {
+        ZIP_EXTENSION_PATTERN = initializePattern(ZIP_EXTENSIONS);
+        GEM_EXTENSION_PATTERN = initializePattern(RUBY_EXTENSIONS);
+        TAR_EXTENSION_PATTERN = initializePattern(TAR_EXTENSIONS);
+    }
+
+    private static String initializePattern(List<String> archiveExtensions) {
+        StringBuilder sb = new StringBuilder();
+        for (String archiveExtension : archiveExtensions) {
+            sb.append(PATTERN_PREFIX);
+            sb.append(archiveExtension);
+            sb.append(OR);
+        }
+        return sb.toString().substring(0, sb.toString().lastIndexOf(OR));
+    }
 
     /* --- Private members --- */
 
-    private String[] javaIncludesPattern;
-    private String[] javaExcludesPattern;
+    private String[] archiveIncludesPattern;
+    private String[] archiveExcludesPattern;
 
     /* --- Constructors --- */
 
     public ArchiveExtractor(String[] archiveIncludes, String[] archiveExcludes) {
         if (archiveIncludes.length > 0 && StringUtils.isNotBlank(archiveIncludes[0])) {
-            this.javaIncludesPattern = archiveIncludes;
+            this.archiveIncludesPattern = archiveIncludes;
         } else {
-            this.javaIncludesPattern = JAVA_EXTENSION;
+            // create ARCHIVE_EXTENSIONS only if archiveIncludes is empty
+            this.archiveIncludesPattern = createArchivesArray();
         }
-        this.javaExcludesPattern = archiveExcludes;
+        this.archiveExcludesPattern = archiveExcludes;
     }
 
     /* --- Public methods --- */
@@ -77,35 +110,89 @@ public class ArchiveExtractor {
 
     /* --- Private methods --- */
 
+    private String[] createArchivesArray() {
+        Collection<String> archiveExtensions = new ArrayList<String>();
+        archiveExtensions.addAll(ZIP_EXTENSIONS);
+        archiveExtensions.addAll(RUBY_EXTENSIONS);
+        archiveExtensions.addAll(TAR_EXTENSIONS);
+
+        String[] archiveIncludesPattern = new String[archiveExtensions.size()];
+        int i = 0;
+        for (String extension : archiveExtensions) {
+            archiveIncludesPattern[i++] = GLOB_PATTERN_PREFIX + extension;
+        }
+        return archiveIncludesPattern;
+    }
+
     private void extractArchive(String scannerBaseDir, String destDirectory, int archiveExtractionDepth, int curLevel) {
         File file = new File(scannerBaseDir);
         if (file.exists()) {
             if (file.isDirectory()) {
+                // scan directory
                 DirectoryScanner scanner = new DirectoryScanner();
                 scanner.setBasedir(scannerBaseDir);
-                scanner.setIncludes(javaIncludesPattern);
-                scanner.setExcludes(javaExcludesPattern);
+                scanner.setIncludes(archiveIncludesPattern);
+                scanner.setExcludes(archiveExcludesPattern);
                 scanner.scan();
+
                 String[] fileNames = scanner.getIncludedFiles();
                 for (String fileName : fileNames) {
-                    if (fileName.matches(JAVA_EXTENSION_PATTERN)) {
-                        try {
-                            String innerDir = destDirectory + File.separator +
-                                    fileName.substring(0, fileName.lastIndexOf(DOT));
-                            File unzipDestination = new File(innerDir);
-                            ZipFile zipFile = new ZipFile(scannerBaseDir + File.separator + fileName);
-                            zipFile.extractAll(unzipDestination.getPath());
-                            if (curLevel < archiveExtractionDepth) {
-                                extractArchive(innerDir, innerDir, archiveExtractionDepth, curLevel + 1);
-                            }
-                        } catch (ZipException e) {
-                            logger.warn("Error extracting file {}: {}", fileName, e.getMessage());
+                    String innerDir = destDirectory + File.separator + fileName.substring(0, fileName.lastIndexOf(DOT));
+                    String archiveFile = scannerBaseDir + File.separator + fileName;
+                        if (fileName.matches(ZIP_EXTENSION_PATTERN)) {
+                            unZip(fileName, innerDir, archiveFile);
+                        } else if (fileName.matches(GEM_EXTENSION_PATTERN)) {
+                            unTar(fileName, innerDir, archiveFile);
+                            innerDir = innerDir + File.separator + RUBY_DATA_FILE ;
+                            unTar(RUBY_DATA_FILE, innerDir.substring(0, innerDir.lastIndexOf(DOT)) , innerDir);
+                            innerDir = innerDir.replaceAll(TAR_GZ_SUFFIX, BLANK);
+                        } else if (fileName.matches(TAR_EXTENSION_PATTERN)) {
+                            unTar(fileName, innerDir, archiveFile);
+                            innerDir = innerDir.replaceAll(TAR_SUFFIX, BLANK);
+                        } else {
+                            logger.warn("Error: {} is unsupported archive type", fileName.substring(fileName.lastIndexOf(DOT)));
+                            return;
                         }
-                    } else {
-                        logger.warn("Error: {} is unsupported archive type", fileName.substring(fileName.lastIndexOf(DOT)));
-                    }
+                        // Extract again if needed according archiveExtractionDepth parameter
+                        if (curLevel < archiveExtractionDepth) {
+                            extractArchive(innerDir, innerDir, archiveExtractionDepth, curLevel + 1);
+                        }
                 }
             }
         }
     }
+
+    // Open and extract data from zip pattern files
+    private void unZip(String fileName, String innerDir, String archiveFile) {
+        try {
+            ZipFile zipFile = new ZipFile(archiveFile);
+            zipFile.extractAll(innerDir);
+        } catch (ZipException e) {
+            logger.warn("Error extracting file {}: {}", fileName, e.getMessage());
+        }
+    }
+
+    // Open and extract data from Tar pattern files
+    private void unTar(String fileName,String innerDir, String archiveFile) {
+        TarUnArchiver unArchiver = new TarUnArchiver();
+        try {
+            if (fileName.endsWith(TAR_GZ_SUFFIX)) {
+                innerDir = innerDir.substring(0, innerDir.lastIndexOf(DOT));
+                unArchiver = new TarGZipUnArchiver();
+            } else if (fileName.endsWith(TAR_SUFFIX) || fileName.endsWith(GEM_EXTENSION_PATTERN)) {
+                unArchiver = new TarUnArchiver();
+            }
+            unArchiver.enableLogging(new ConsoleLogger(ConsoleLogger.LEVEL_DISABLED, UN_ARCHIVER_LOGGER));
+            unArchiver.setSourceFile(new File(archiveFile));
+            File destDir = new File(innerDir);
+            if (!destDir.exists()){
+                destDir.mkdirs();
+            }
+            unArchiver.setDestDirectory(destDir);
+            unArchiver.extract();
+        } catch (Exception e) {
+            logger.warn("Error extracting file {}: {}", fileName, e.getMessage());
+        }
+    }
+
 }
