ddc0ac885ee9512a9fb43c268255ab60a4a60539
Chen <chen.luigi@whitesourcesoftware.com>
2018-01-22 15:19:17 +0200

    Change the name of the command -whiteSourceFolder to -whiteSourceFolderPath

--- src/main/java/org/whitesource/fs/CommandLineArgs.java
+++ src/main/java/org/whitesource/fs/CommandLineArgs.java
@@ -89,7 +89,7 @@ public class CommandLineArgs {
     @Parameter(names = "-web", description = "Whether or not to create a web service on startup that receives requests from outside")
     String web = "false";
 
-    @Parameter(names = "-whiteSourceFolder", description = "WhiteSource folder path for offlineRequest/checkPolicies")
+    @Parameter(names = "-whiteSourceFolderPath", description = "WhiteSource folder path for offlineRequest/checkPolicies")
     String whiteSourceFolder = null;
 
     /* --- Public methods --- */
