56893bcbf6c7c8b87582b403a51a773a485fd05c
eRez-ws <35656848+eRez-ws@users.noreply.github.com>
2018-03-29 11:01:11 +0300

    removed @Ignore

--- src/test/java/org/whitesource/agent/dependency/resolver/gradle/GradleLinesParserTest.java
+++ src/test/java/org/whitesource/agent/dependency/resolver/gradle/GradleLinesParserTest.java
@@ -22,7 +22,6 @@ public class GradleLinesParserTest {
     }
 
     @Test
-    @Ignore
     public void parseLines() throws IOException {
         String[] params = new String[] {"cmd", "/c", "gradle", "dependencies"};
         String folderPath = Paths.get(".").toAbsolutePath().normalize().toString() + TestHelper.getOsRelativePath("\\src\\test\\resources\\resolver\\gradle\\sample\\");
