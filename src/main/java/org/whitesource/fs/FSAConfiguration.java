9bea6dc59d6a89e9827e65f8aec8ae7e9d60bd7a
annarozin <anna.rozin@whitesourcesoftware.com>
2018-05-16 09:46:31 +0300

    WSE-460 - scan current running directory in case scanned directory is not set by the user

--- src/main/java/org/whitesource/fs/FSAConfiguration.java
+++ src/main/java/org/whitesource/fs/FSAConfiguration.java
@@ -162,6 +162,11 @@ public class FSAConfiguration {
         scanPackageManager = getBooleanProperty(config, SCAN_PACKAGE_MANAGER, false);
         scanDockerImages = getBooleanProperty(config,SCAN_DOCKER_IMAGES,false);
 
+        // validate scanned folder
+        if (dependencyDirs.isEmpty()) {
+            dependencyDirs.add(".");
+        }
+
         // validate config
         String projectToken = config.getProperty(PROJECT_TOKEN_PROPERTY_KEY);
         String projectNameFinal = !StringUtils.isBlank(projectName) ? projectName : config.getProperty(PROJECT_NAME_PROPERTY_KEY);
@@ -259,7 +264,7 @@ public class FSAConfiguration {
 
         boolean dependenciesOnly            = FSAConfiguration.getBooleanProperty(config, DEPENDENCIES_ONLY, false);
 
-        String whitesourceConfiguration     = config.getProperty(PROJECT_CONFIGURATION_PATH);
+        String whiteSourceConfiguration     = config.getProperty(PROJECT_CONFIGURATION_PATH);
 
         boolean pythonResolveDependencies           = FSAConfiguration.getBooleanProperty(config, PYTHON_RESOLVE_DEPENDENCIES, true);
         String pipPath                              = config.getProperty(PYTHON_PIP_PATH, PIP);
@@ -284,7 +289,7 @@ public class FSAConfiguration {
                 bowerResolveDependencies, bowerRunPreStep, nugetResolveDependencies, nugetRestoreDependencies,
                 mavenResolveDependencies, mavenIgnoredScopes, mavenAggregateModules,
                 pythonResolveDependencies, pipPath, pythonPath, pythonIsWssPluginInstalled, pythonUninstallWssPluginInstalled,
-                dependenciesOnly, whitesourceConfiguration, gradleResolveDependencies, gradleRunAssembleCommand, paketResolveDependencies, paketIgnoredScopes, paketIgnoreFiles, paketRunPreStep, paketPath,
+                dependenciesOnly, whiteSourceConfiguration, gradleResolveDependencies, gradleRunAssembleCommand, paketResolveDependencies, paketIgnoredScopes, paketIgnoreFiles, paketRunPreStep, paketPath,
                 goResolveDependencies, goDependencyManager, goCollectDependenciesAtRuntime);
     }
 
