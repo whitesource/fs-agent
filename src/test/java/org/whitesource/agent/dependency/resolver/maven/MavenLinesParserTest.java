a55f27a2a67653e3b4c8c8a0cee9cfc53702d121
eRez-ws <35656848+eRez-ws@users.noreply.github.com>
2018-08-15 10:50:26 +0300

    WSE-730 - resolving maven projects where verbose=true

--- src/test/java/org/whitesource/agent/dependency/resolver/maven/MavenLinesParserTest.java
+++ src/test/java/org/whitesource/agent/dependency/resolver/maven/MavenLinesParserTest.java
@@ -2,6 +2,7 @@ package org.whitesource.agent.dependency.resolver.maven;
 
 import fr.dutra.tools.maven.deptree.core.Node;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.whitesource.agent.dependency.resolver.npm.TestHelper;
@@ -12,15 +13,37 @@ import java.util.List;
 
 public class MavenLinesParserTest {
 
+    private MavenLinesParser mavenLinesParser;
+    @Before
+    public void setUp(){
+        mavenLinesParser = new MavenLinesParser();
+    }
     @Ignore
     @Test
     public void shouldParseLinesWithWarnings(){
         File file = TestHelper.getFileFromResources("resolver/maven/lines.txt");
         List<String> lines = readFileAsList(file.getAbsolutePath());
-        MavenLinesParser mavenLinesParser = new MavenLinesParser();
         List<Node> nodes = mavenLinesParser.parseLines(lines);
         Assert.assertEquals(nodes.size() , 1);
         Assert.assertEquals(nodes.get(0).getArtifactId(),"junrar");
+    }
+
+    @Ignore
+    @Test
+    public void parseVerboseLines(){
+        File file = TestHelper.getFileFromResources("resolver/maven/verboseLines.txt");
+        List<String> lines = readFileAsList(file.getAbsolutePath());
+        List<Node> nodes = mavenLinesParser.parseLines(lines);
+        Assert.assertTrue(nodes.size() > 0);
+    }
+
+    @Ignore
+    @Test
+    public void parseLines(){
+        File file = TestHelper.getFileFromResources("resolver/maven/lines2.txt");
+        List<String> lines = readFileAsList(file.getAbsolutePath());
+        List<Node> nodes = mavenLinesParser.parseLines(lines);
+        Assert.assertTrue(nodes.size() > 0);
     }
 
     private List<String> readFileAsList(String fileName) {
