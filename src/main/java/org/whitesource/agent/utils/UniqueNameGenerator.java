adef7f2627c9a2e719aae730731a192b1682b8d3
Raz Nitzan <raz.nitzan@whitesourcesofware.com>
2018-11-28 11:53:02 +0200

    minor change

--- src/main/java/org/whitesource/agent/utils/UniqueNameGenerator.java
+++ src/main/java/org/whitesource/agent/utils/UniqueNameGenerator.java
@@ -9,13 +9,13 @@ import java.util.UUID;
 /**
  * @author raz.nitzan
  */
-public class UniqueSuffixGenerator {
+public class UniqueNameGenerator {
 
     /* --- public static methods --- */
 
-    public static String createUniqueSuffix(String name) {
+    public static String createUniqueName(String name, String extension) {
         String creationDate = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         String uuid = UUID.randomUUID().toString();
-        return name + Constants.UNDERSCORE + creationDate + Constants.UNDERSCORE + uuid;
+        return name + Constants.UNDERSCORE + creationDate + Constants.UNDERSCORE + uuid + extension;
     }
 }
